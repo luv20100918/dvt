d8f043a2cdaeac271fe96553cecd5898f5b1041f
ohchoongryeol <luv20100918@gmail.com>
2022-09-02 08:25:17 +0900

    .

--- src/App.js
+++ src/App.js
@@ -1,23 +1,31 @@
-import logo from './logo.svg';
+//import logo from "./logo.svg";
 import './App.css';
+import DVT31 from './DVT31';
+import { useState } from 'react';
 
 function App() {
+
+  function getRandomArbitrary(min, max) {
+    min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min)) + min;
+  }
+
+  const [type , setType] = useState('question');
+
+  const questions = [];
+  for( let i = 0 ; i < 9 ; i++) {
+    questions.push({
+      no : i+1, d1 : getRandomArbitrary(100, 999), d2 : getRandomArbitrary(2, 9)
+    });
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="page">
+      <header><span><button onClick={()=>{setType('answer');}}>답안</button></span></header>
+      {questions.map((question, i)=> {
+        return <DVT31 key={i} no={question.no} type={type} d1={question.d1} d2={question.d2} />  
+      })}
     </div>
   );
 }
